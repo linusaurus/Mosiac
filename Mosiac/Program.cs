10ca421fbd1f60257f56ddc21439f2c937504b6c
Richard Young <rich@designsynthesis.net>
2017-10-26 16:36:26 -0700

    Add project files.

---
+++ Mosiac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mosiac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
